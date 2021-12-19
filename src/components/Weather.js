fa188549b6db957f0cc17573b7767987ac84ec02
Mariano Moran <marianomoran@Marianos-MacBook-Pro.local>
2021-12-18 19:10:54 -0500

    setting up api key and queries

---
+++ src/components/Weather.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+
+const api = {
+  key: "5816b06f09fef7a89c2fb77399b2e021",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function Weather() {
+    const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  
+  
+}
+
+export default Weather;
