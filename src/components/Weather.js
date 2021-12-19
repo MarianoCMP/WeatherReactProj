0c07a4209cf0dbe14d43cc1eb9a395f91e73caf1
Mariano Moran <marianomoran@Marianos-MacBook-Pro.local>
2021-12-18 21:57:58 -0500

    missing css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -22,18 +22,47 @@ function Weather() {
     }
   }
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
-
+        return `${day} ${date} ${month} ${year}`
+    }
+    return (
+        <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+          <main>
+            <div className="search-box">
+              <input 
+                type="text"
+                className="search-bar"
+                placeholder="Search..."
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+              />
+            </div>
+            {(typeof weather.main != "undefined") ? (
+            <div>
+              <div className="location-box">
+                <div className="location">{weather.name}, {weather.sys.country}</div>
+                <div className="date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">
+                  {Math.round(weather.main.temp)}Â°c
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+            </div>
+            ) : ('')}
+          </main>
+        </div>
+      );
   
   
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import AboutMe from './components/AboutMe';
+import Home from './components/Home';
+import Projects from './components/Projects';
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+        
+        < Route path="" element={<App />} >
+
+        <Route path = "Home" element= {<Home/>} />
+
+        < Route path="Projects" element={<Projects />} >
+
+         < Route path="VideoGame" element={<VideoGame/>} />
+        < Route path = "SoloVideoGame" element={<SoloVideoGame/>}/>
+        <Route path = "MobileApp" element = {< MobileApp/>} />
+        
+        </Route> 
+
+        < Route path="ContactMe" element={<ContactMe />} />
+
+        < Route path = "AboutMe" element = {<AboutMe/>} />
+    
+        </Route>
+        </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
